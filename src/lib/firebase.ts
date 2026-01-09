656d7b469cdf73512a43f7de99153c9d746d0cca
Edwin Aguiar <fenixlife1978@gmail.com>
2026-01-09 01:30:42 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-9155034390-c4587",
+  "appId": "1:500254308818:web:774dcae7bfc629100bb00b",
+  "apiKey": "AIzaSyBw5pP6CF0M49vWv2ZzytIV-UQDcjw-5Tc",
+  "authDomain": "studio-9155034390-c4587.firebaseapp.com",
+  "storageBucket": "studio-9155034390-c4587.appspot.com",
+  "messagingSenderId": "500254308818"
 };
 
 // Initialize Firebase
