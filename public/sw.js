c870f8428010b6e060fc5bfc0a4c6d722dd9b665
Edwin Aguiar <fenixlife1978@gmail.com>
2026-01-11 20:41:48 +0000

    service worker

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,10 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
+// Service Worker con experiencia offline (página de respaldo + copia de páginas)
 const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
-const offlineFallbackPage = "ToDo-replace-this-name.html";
+// Página de respaldo offline
+const offlineFallbackPage = "offline.html";
 
 self.addEventListener("message", (event) => {
   if (event.data && event.data.type === "SKIP_WAITING") {
@@ -36,15 +35,11 @@ self.addEventListener('fetch', (event) => {
     event.respondWith((async () => {
       try {
         const preloadResp = await event.preloadResponse;
-
-        if (preloadResp) {
-          return preloadResp;
-        }
+        if (preloadResp) return preloadResp;
 
         const networkResp = await fetch(event.request);
         return networkResp;
       } catch (error) {
-
         const cache = await caches.open(CACHE);
         const cachedResp = await cache.match(offlineFallbackPage);
         return cachedResp;
