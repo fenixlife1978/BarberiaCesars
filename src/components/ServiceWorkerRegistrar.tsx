fd0b74aa70a6c48accf8ba0403769360b3640b8a
Edwin Aguiar <fenixlife1978@gmail.com>
2026-01-11 21:02:54 +0000

    service corregido

--- src/components/ServiceWorkerRegistrar.tsx
+++ src/components/ServiceWorkerRegistrar.tsx
@@ -1,17 +1,18 @@
-'use client';
+"use client";
 
-import { useEffect } from 'react';
+import { useEffect } from "react";
 
 export default function ServiceWorkerRegistrar() {
   useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then(registration => {
-          console.log('SW registered: ', registration);
-        }).catch(registrationError => {
-          console.log('SW registration failed: ', registrationError);
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/sw.js")
+        .then((registration) => {
+          console.log("✅ Service Worker registrado con éxito:", registration);
+        })
+        .catch((error) => {
+          console.error("❌ Error al registrar el Service Worker:", error);
         });
-      });
     }
   }, []);
 
